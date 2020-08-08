bd957d0b6238d937caa2ab7905b521d5dab17f01
Houssam Salem <houssam.salem.au@gmail.com>
2020-08-08 22:14:31 +1000

    Fix handling negated terms
    From "-is:due" to -"is:due"

--- __init__.py
+++ __init__.py
@@ -10,12 +10,20 @@ import shlex
 def willSearch(ctx: SearchContext):
     terms = shlex.split(ctx.search)
     processed = []
+    negated = []
     for term in terms:
+        if term[0] == "-":
+            term = term[1:]
+            negated.append(term)
+            continue
         if ':' in term:
             processed.append(term)
         else:
             processed.append("nc:" + term)
     ctx.search = ' '.join('"{0}"'.format(t) for t in processed)
+    ctx.search += ' '
+    ctx.search += ' '.join('-"{0}"'.format(t) for t in negated)
+    print(ctx.search)
     return ''
 
 gui_hooks.browser_will_search.append(willSearch)
